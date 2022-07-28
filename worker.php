adde3bc589c47dab6c287d091a951593bf6b8e23
Batuhan Özen <batuhanozen06@gmail.com>
2022-07-28 23:04:16 +0300

    update worke
    insert a switch case into worker. Get method parameter from request. Next step is processing request

--- worker.php
+++ worker.php
@@ -1,7 +1,6 @@
 <?php
 use Workerman\Worker;
 require_once './vendor/autoload.php';
-
 // Create A Worker and Listens 3000 port, use Websocket protocol
 $worker = new Worker("websocket://127.0.0.3:3000");
 
@@ -11,27 +10,46 @@ $worker->count = 1;
 // Emitted when new connection come
 $worker->onConnect = function($connection){
     // Emitted when websocket handshake done
-    $connection->onWebSocketConnect = function($connection,$request){
+    /* $connection->onWebSocketConnect = function($connection,$request){
         echo "New connection.\nipAddres => " . $connection->getRemoteIp() . "\n";
         echo"\n\n";
-    };
+    }; */
     $connection->onMessage = function($connection, $d){
+        echo "\n\n";
+        global $validMethods;
         $data = json_decode($d);
         if(isset($data->method) && isset($data->data)){
             $connection->send('received successfully.');
-            var_dump($data->data);
+                switch ($data->method) {
+                    case 'create':
+                        echo "new create request";
+                        break;
+                    case 'insert':
+                        echo "new insert request";
+                        break;
+                    case 'update':
+                        echo "new update request";
+                        break;
+                    case 'get':
+                        echo "invalid request:";
+                        break;
+                    default:
+                        echo "invalid request: ";
+                        $connection->send('invalid request!');
+                        break;
+                }
         }
         else{
             $connection->send('invalid request!');
         }
-        
+        $connection->close('connection closed');
     };
 };
 
 
 // Emitted when connection closed
 $worker->onClose = function($connection){
-    echo "Connection closed";
+    echo "\nConnection closed";
 };
 
 // Run worker
